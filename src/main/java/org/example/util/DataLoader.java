aad00c7434fe29b31c436bcebd334c6e572562aa
Pavel Borodin <borpavbor122@gmail.com>
2023-03-16 15:32:55 +0500

    resolved the problem of manifest attribute

--- src/main/java/org/example/util/DataLoader.java
+++ src/main/java/org/example/util/DataLoader.java
@@ -1,31 +1,30 @@
 package org.example.util;
 
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
 public class DataLoader {
-    public List<String> getDataByColumn(int columnNumber) throws FileNotFoundException {
+    public List<String> getDataByColumn(int columnNumber) throws IOException {
 
-        FileInputStream fileInputStream = new FileInputStream(".\\airports.csv");
-        BufferedReader reader = new BufferedReader(new InputStreamReader(fileInputStream));
+        try (InputStream inputStream = getClass().getResourceAsStream("/airports.csv")) {
+            assert inputStream != null;
+            try (BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream))) {
+                String splitCSV = ",";
 
-        String splitCSV = ",";
-
-        List<String> lines = new ArrayList<>();
-        try {
-            String str;
-            while ((str = reader.readLine()) != null) {
-                String[] airportData = str.replaceAll("\"", "").split(splitCSV);
-                lines.add(airportData[columnNumber - 1]);
+                List<String> lines = new ArrayList<>();
+                try {
+                    String str;
+                    while ((str = reader.readLine()) != null) {
+                        String[] airportData = str.replaceAll("\"", "").split(splitCSV);
+                        lines.add(airportData[columnNumber - 1]);
+                    }
+                }
+                catch (Exception e){
+                    e.printStackTrace();
+                }
+                return lines;// Use resource
             }
         }
-        catch (Exception e){
-            e.printStackTrace();
-        }
-        return lines;
     }
 }
